00c104067f96e6329cbaaab9a6ce539c49c86cc6
Till Junge <till.junge@epfl.ch>
2019-02-28 22:10:29 +0100

    CI: cleaned up dockerfiles for new CI
    [ci skip]

---
+++ dockerfiles/update_dockers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cd debian:stable/
+docker build -t registry.gitlab.com/tjunge/muspectre:debian_stable .
+docker push registry.gitlab.com/tjunge/muspectre:debian_stable
+cd -
+
+cd debian:testing/
+docker build -t registry.gitlab.com/tjunge/muspectre:debian_testing .
+docker push registry.gitlab.com/tjunge/muspectre:debian_testing
+cd -
+
+cd ubuntu:16.04/
+docker build -t registry.gitlab.com/tjunge/muspectre:ubuntu16.04 .
+docker push registry.gitlab.com/tjunge/muspectre:ubuntu16.04
+cd -
