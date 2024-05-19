29555568b71bbf94d1d2047d26e6bac733d6f854
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-19 22:23:09 +0200

    CI: Static library

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -1,5 +1,5 @@
 curl https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
 cd netcdf-c-4.9.2
-./configure --disable-hdf5
+./configure --disable-hdf5 --disable-shared
 make
 make install
