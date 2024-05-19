a0de2f8e454ac4b5becace86325875e995d5e4d2
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-19 22:55:52 +0200

    CI: Install libcurl-devel

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -1,3 +1,4 @@
+yum install -y libcurl-devel
 curl https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
 cd netcdf-c-4.9.2
 ./configure --disable-hdf5 --disable-shared --disable-libxml2
