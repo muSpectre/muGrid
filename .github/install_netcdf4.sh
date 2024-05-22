a25c95b96cb2a96aa00a90a482ac3c9f295722eb
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-22 19:08:22 +0000

    CI: Build libxml2 as static library

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -8,12 +8,12 @@ fi
 printenv
 curl -L https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz | tar -Jx
 cd libxml2-2.12.7
-./configure --without-python --prefix=$INSTALL_PREFIX
+./configure --without-lzma --without-python --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
 cd ..
 curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
 cd netcdf-c-4.9.2
-./configure --disable-hdf5 --disable-byterange --disable-shared --with-pic --prefix=$INSTALL_PREFIX
+./configure --disable-hdf5 --disable-byterange --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
