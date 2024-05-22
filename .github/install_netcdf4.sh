d7381a054081e018b585159afee567d52bd07a57
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-22 16:48:38 +0000

    CI: Decide on install prefix depending on whether homebrew is present

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -1,12 +1,18 @@
 printenv
+if [ -z "$HOMEBREW_REPOSITORY" ]; then
+    INSTALL_PREFIX=/usr/local
+else
+    INSTALL_PREFIX=$HOMEBREW_REPOSITORY
+fi
+echo "INSTALL_PREFIX=$INSTALL_PREFIX"
 curl -L https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz | tar -Jx
 cd libxml2-2.12.7
-./configure --without-python --prefix=$HOME/.local
+./configure --without-python --prefix=$INSTALL_PREFIX
 make
 make install
 cd ..
 curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
 cd netcdf-c-4.9.2
-PATH=$PATH:$HOME/.local/bin ./configure --disable-hdf5 --disable-byterange --disable-shared --with-pic --prefix=$HOME/.local
+./configure --disable-hdf5 --disable-byterange --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
