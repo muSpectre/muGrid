c35ee18f70f6ed0702c18a481962ae64c2d99333
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-22 15:51:48 +0000

    CI: Install into HOME

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -1,10 +1,12 @@
+printenv
 curl -L https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz | tar -Jx
 cd libxml2-2.12.7
-./configure --without-python
+./configure --without-python --prefix=$HOME/.local
 make
 make install
+cd ..
 curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
 cd netcdf-c-4.9.2
-./configure --disable-hdf5 --disable-byterange --disable-shared --with-pic
+./configure --disable-hdf5 --disable-byterange --disable-shared --with-pic --prefix=$HOME/.local
 make
 make install
