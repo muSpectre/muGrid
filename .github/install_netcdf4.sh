c145b7825483173d50e8c1f7f67ad0c67e240ba9
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-22 15:43:48 +0000

    CI: Compile xml2 before compiling NetCDF4

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -1,6 +1,10 @@
-yum install -y libcurl-devel
-curl https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
+curl -L https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz | tar -Jx
+cd libxml2-2.12.7
+./configure --without-python
+make
+make install
+curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz | tar -zx
 cd netcdf-c-4.9.2
-./configure --disable-hdf5 --disable-shared --disable-libxml2 --with-pic
+./configure --disable-hdf5 --disable-byterange --disable-shared --with-pic
 make
 make install
