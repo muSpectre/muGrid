abeab4d31dec774bd5599048ff882465e7f14533
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-06-01 22:48:05 +0200

    DOC: Added I/O example

---
+++ examples/io.py
@@ -0,0 +1,21 @@
+import numpy as np
+from muGrid import GlobalFieldCollection, FileIONetCDF, OpenMode
+
+# Two dimensional grid
+nb_grid_pts = (11, 12, 13)
+fc = GlobalFieldCollection(nb_grid_pts, sub_pts={'element': 5})
+
+# Get a tensor-field (for example to represent the strain)
+strain = fc.real_field(
+    'strain',  # name of the field
+    (3, 3),  # shape of components
+    'element'  # sub-point type
+)
+
+# Fill the field with random numbers
+strain.s = np.random.rand(*((3, 3, 5) + nb_grid_pts))
+
+# Initialize a file I/O object
+file = FileIONetCDF('example.nc', OpenMode.Overwrite)
+file.register_field_collection(fc)
+file.append_frame().write()  # Write all fields of the field collection to the file
