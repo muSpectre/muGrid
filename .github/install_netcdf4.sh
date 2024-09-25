c804504210a020d442613d334fa79a7dcca301b1
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-09-25 21:15:46 +0200

    Revert "CI: Install libcurl"
    This reverts commit c705337847bd9922b9917a3523802c2f41750ed4.

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -25,15 +25,6 @@ echo "Installing into prefix ${INSTALL_PREFIX}..."
 echo "PATH is ${PATH}"
 echo "PKG_CONFIG_PATH is ${PKG_CONFIG_PATH}"
 
-CURL="curl-8.10.1"
-curl -L https://curl.se/download/${CURL}.tar.xz | tar -Jx
-mkdir ${CURL}-build
-cd ${CURL}-build
-cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX ../${CURL}
-make
-make install
-cd ..
-
 XML2="libxml2-2.12.9"
 curl -L https://download.gnome.org/sources/libxml2/2.12/${XML2}.tar.xz | tar -Jx
 mkdir ${XML2}-build
@@ -47,6 +38,6 @@ NETCDF=netcdf-c-4.9.2
 curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/${NETCDF}.tar.gz | tar -zx
 mkdir ${NETCDF}-build
 cd ${NETCDF}-build
-cmake -DENABLE_TESTS=OFF -DENABLE_HDF5=OFF -DENABLE_NETCDF4=OFF -DENABLE_NETCDF_4=OFF -DENABLE_CDF5=ON -DENABLE_BYTERANGE=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX ../${NETCDF}
+cmake -DENABLE_TESTS=OFF -DENABLE_HDF5=OFF -DENABLE_CDF5=OFF -DENABLE_BYTERANGE=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX ../${NETCDF}
 make
 make install
