4c07157b8b9a9e924d3620381a35dd5dc2651bf2
Lars Pastewka <lars.pastewka@surface.design>
2024-09-25 17:54:06 +0200

    CI: Explicitly specify target arch

--- .github/install_netcdf4.sh
+++ .github/install_netcdf4.sh
@@ -8,12 +8,14 @@ echo "=========================="
 
 # The variable HOMEBREW_REPOSITORY appears to be only present on ARM Macs, not Intel Macs
 # HOMEBREW_NO_AUTO_UPDATE is present on both
+if [ -z "$INSTALL_PREFIX" ]; then
 if [ -z "$HOMEBREW_NO_AUTO_UPDATE" ]; then
     INSTALL_PREFIX=/usr/local
 else
     brew install pkg-config
     INSTALL_PREFIX=$(which brew | sed 's,/bin/brew,,')
     # libxml2 pkg-config file is not in the default path on Intel Macs
+fi
 fi
 
 export PATH=${INSTALL_PREFIX}/bin:${PATH}
@@ -26,7 +28,7 @@ echo "PKG_CONFIG_PATH is ${PKG_CONFIG_PATH}"
 XML2="libxml2-2.12.9"
 curl -L https://download.gnome.org/sources/libxml2/2.12/${XML2}.tar.xz | tar -Jx
 cd ${XML2}
-./configure --without-lzma --without-python --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
+./configure --build=$(echo $ARCHFLAGS | sed 's/-arch //') --without-lzma --without-python --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
 cd ..
@@ -34,6 +36,6 @@ cd ..
 NETCDF=netcdf-c-4.9.2
 curl -L https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/${NETCDF}.tar.gz | tar -zx
 cd ${NETCDF}
-./configure --disable-hdf5 --disable-byterange --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
+./configure --build=$(echo $ARCHFLAGS | sed 's/-arch //') --disable-hdf5 --disable-byterange --enable-static --disable-shared --with-pic --prefix=$INSTALL_PREFIX
 make
 make install
