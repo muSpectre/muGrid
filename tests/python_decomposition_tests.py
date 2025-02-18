6df240cb2a9b1a8fc63a04eb40b3821e1b01dcf2
Yizhen Wang <yizhen.wang@imtek.uni-freiburg.de>
2025-02-18 18:53:59 +0100

    TST: write the python test without a reference of the whole domain

--- tests/python_decomposition_tests.py
+++ tests/python_decomposition_tests.py
@@ -19,22 +19,19 @@ class DecompositionCheck(unittest.TestCase):
             raise NotImplementedError("Not planned for this number of processes.")
 
     def test_communicate_ghost(self):
+        # Create a communicator
         try:
             from mpi4py import MPI
             comm = muGrid.Communicator(MPI.COMM_WORLD)
         except ImportError:
             comm = muGrid.Communicator()
         nb_processes = comm.size
-        nb_subdivisions = self.get_nb_subdivisions(nb_processes)
 
+        # Create a Cartesian decomposition
+        nb_subdivisions = self.get_nb_subdivisions(nb_processes)
         spatial_dim = len(nb_subdivisions)
         nb_pts_per_dim = 5
         nb_domain_grid_pts = np.full(spatial_dim, nb_pts_per_dim)
-
-        ref_values = np.reshape(
-            np.arange(nb_pts_per_dim**spatial_dim), nb_domain_grid_pts
-        )
-
         nb_ghost_left = np.full(spatial_dim, 1)
         nb_ghost_right = np.full(spatial_dim, 2)
         cart_decomp = muGrid.CartesianDecomposition(
@@ -45,13 +42,17 @@ class DecompositionCheck(unittest.TestCase):
             nb_ghost_right.tolist(),
         )
 
+        # Create a field for testing
         field_name = "test_field"
         field = cart_decomp.collection.real_field(field_name)
 
-        nb_subdomain_grid_pts = cart_decomp.nb_subdomain_grid_pts
+        # Create reference values
         global_coords = cart_decomp.global_coords
+        weights = np.arange(spatial_dim) + 1
+        ref_values = np.einsum("i, i...->...", weights, global_coords)
 
-        # Fill non-ghost cells
+        # Fill the field, non-ghost with reference values, ghost with some other value
+        nb_subdomain_grid_pts = cart_decomp.nb_subdomain_grid_pts
         for index in np.ndindex(*nb_subdomain_grid_pts):
             is_not_ghost = all(
                 idx >= nb_ghost_left[dim]
@@ -59,20 +60,18 @@ class DecompositionCheck(unittest.TestCase):
                 for dim, idx in enumerate(index)
             )
             if is_not_ghost:
-                ref_index = global_coords[(..., *index)]
-                field.s[(..., *index)] = ref_values[tuple(ref_index)]
+                field.s[(..., *index)] = ref_values[(..., *index)]
             else:
                 field.s[(..., *index)] = -1
 
         # Communicate ghost cells
         cart_decomp.communicate_ghosts(field_name)
 
-        # Validate ghost cells
+        # Check values at all grid points
         for index in np.ndindex(*nb_subdomain_grid_pts):
-            ref_index = global_coords[(..., *index)]
             self.assertEqual(
                 field.s[(..., *index)],
-                ref_values[tuple(ref_index)],
+                ref_values[(..., *index)],
                 f"Mismatch at {index}",
             )
 
