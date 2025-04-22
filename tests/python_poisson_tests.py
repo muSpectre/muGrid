a9c3527c7e7595f1457750fbf502c86694634d26
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-04-22 08:03:34 +0200

    TST: Decomposition test

--- tests/python_poisson_tests.py
+++ tests/python_poisson_tests.py
@@ -1,8 +1,17 @@
-import unittest
 import numpy as np
+from NuMPI.Testing.Subdivision import suggest_subdivisions
 
 import muGrid
 
 
-def test_poisson_solver(comm):
-    print(comm.rank, comm.size)
+def test_poisson_solver(comm, nb_grid_pts=(32, 32)):
+    s = suggest_subdivisions(len(nb_grid_pts), comm.size)
+
+    decomposition = muGrid.CartesianDecomposition(comm, nb_grid_pts, s, (1, 1), (1, 1))
+
+    stencil = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+    laplace = muGrid.ConvolutionOperator(len(nb_grid_pts), stencil)
+    assert laplace.nb_operators == 1
+    assert laplace.nb_quad_pts == 1
+
+    assert decomposition.nb_subdivisions == s
